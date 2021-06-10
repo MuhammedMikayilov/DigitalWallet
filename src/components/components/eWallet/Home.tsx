3818e409719ca7d8e59a6a2f4a506b93f1f5d26e
Muhammed Mikayilov <muhammedmikayilov@Muhammed-MacBook-Pro.local>
2021-06-10 11:04:18 +0400

    Fixed conflict problem

--- src/components/components/eWallet/Home.tsx
+++ src/components/components/eWallet/Home.tsx
@@ -22,7 +22,6 @@ import {
 } from "@material-ui/core/styles";
 // import { useHistory } from "react-router-dom";
 import Cards from "./Cards";
-// import AddCard from "./AddCard";
 import { useHistory } from "react-router";
 import { useDispatch } from "react-redux";
 import { getCardsDetail } from "../../redux/actions/cards/cardsAction";
